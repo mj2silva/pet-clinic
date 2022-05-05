dc11112af5f661e9a2313957e00fa8c2fe9724ee
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-05 13:03:40 -0500

    Added interfaces for PetService, VetService and OwnerService.

---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/PetService.java
@@ -0,0 +1,11 @@
+package com.msilva.petclinic.services;
+
+import com.msilva.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/VetService.java
@@ -0,0 +1,11 @@
+package com.msilva.petclinic.services;
+
+import com.msilva.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet owner);
+    Set<Vet> findAll();
+}
