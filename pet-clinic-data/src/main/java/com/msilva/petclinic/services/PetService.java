45c276b6cc702ac4bb9e4dbcbb928749c26d833d
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-05 23:22:28 -0500

    Refactor services to use common interface

--- pet-clinic-data/src/main/java/com/msilva/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/PetService.java
@@ -4,8 +4,6 @@ import com.msilva.petclinic.model.Pet;
 
 import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet owner);
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
+
 }
--- pet-clinic-data/src/main/java/com/msilva/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/VetService.java
@@ -2,10 +2,6 @@ package com.msilva.petclinic.services;
 
 import com.msilva.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
