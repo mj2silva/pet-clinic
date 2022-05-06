4698951ef06c332e35fbaddaf7c12191d1c3dd74
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-06 00:14:53 -0500

    Added vets index page and vet controller

---
+++ pet-clinic-web/src/main/java/com/msilva/petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.msilva.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping("/vets")
+    public String listVets() {
+        return "vets/index";
+    }
+}
