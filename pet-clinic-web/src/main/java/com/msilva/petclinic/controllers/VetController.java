20c9ff55726bf0033a6c6dabf3dde617d9d6c4b0
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-07 02:03:23 -0500

    List of owners and vets with map implementation of services

--- pet-clinic-web/src/main/java/com/msilva/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/msilva/petclinic/controllers/VetController.java
@@ -1,14 +1,23 @@
 package com.msilva.petclinic.controllers;
 
+import com.msilva.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", ""})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
