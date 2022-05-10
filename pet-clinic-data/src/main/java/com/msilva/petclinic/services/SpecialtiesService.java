8fd7b206f79adf4481154ab7d067a6d85a813712
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-09 21:39:13 -0500

    Implementation of SpecialtiesService interface with map

---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package com.msilva.petclinic.services;
+
+import com.msilva.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/map/SpecialtiesServiceMap.java
@@ -0,0 +1,35 @@
+package com.msilva.petclinic.services.map;
+
+import com.msilva.petclinic.model.Specialty;
+import com.msilva.petclinic.services.SpecialtiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtiesServiceMap extends AbstractMapService<Specialty, Long> implements SpecialtiesService {
+    @Override
+    public Set<Specialty> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Specialty findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Specialty save(Specialty object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(Specialty object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
