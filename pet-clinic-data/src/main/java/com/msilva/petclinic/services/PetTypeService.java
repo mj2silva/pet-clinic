4a70116a610949544712906f1cf72fdf0ba59ca2
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-09 20:20:20 -0500

    Added PetTypeService implementation with map

---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.msilva.petclinic.services;
+
+import com.msilva.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,33 @@
+package com.msilva.petclinic.services.map;
+
+import com.msilva.petclinic.model.PetType;
+import com.msilva.petclinic.services.PetTypeService;
+
+import java.util.Set;
+
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void delete(PetType object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
