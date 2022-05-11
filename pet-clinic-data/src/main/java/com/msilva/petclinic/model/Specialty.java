330bb826c4221ff1d2e3aa65d0bbd90dc97f9f7c
Manuel Silva <manuel.silvag1@gmail.com>
2022-05-11 00:58:51 -0500

    Added Specialty and Vet JPA Annotations

--- pet-clinic-data/src/main/java/com/msilva/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/model/Specialty.java
@@ -1,12 +1,14 @@
 package com.msilva.petclinic.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Specialty extends BaseEntity {
     private String name;
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
--- pet-clinic-data/src/main/java/com/msilva/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/msilva/petclinic/model/Vet.java
@@ -1,9 +1,17 @@
 package com.msilva.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
 public class Vet extends Person {
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "vet_specialty",
+            joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "specialty_id"
+            ))
     private Set<Specialty> specialties;
 
     public Set<Specialty> getSpecialties() {
